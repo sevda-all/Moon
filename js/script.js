135c9f98b394fe1d1f21e705f82805586635ac46
Sevda Allakhverdieva <56334743+sevda-all@users.noreply.github.com>
2022-11-02 23:03:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+}
+
 let stars =  document.getElementById('stars');
 let moon =  document.getElementById('moon');
 let mountains_behind =  document.getElementById('mountains_behind');
